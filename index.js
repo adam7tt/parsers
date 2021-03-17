a59443d8e3a134916aeefe16af158b17f0214e8b
adam7tt <adam7tt@gmail.com>
2021-03-16 17:21:04 -0700

    parsers

---
+++ index.js
@@ -0,0 +1,13 @@
+// const xlsx = require('node-xlsx')
+
+const ExcelJS = require('exceljs');
+
+
+// const workSheetsFromFile = xlsx.parse(`./prod_report_2021.xlsx`);
+// console.log(workSheetsFromFile[0].data)
+
+
+const workbook = new ExcelJS.Workbook();
+workbook.xlsx.readFile('./prod_report_2021.xlsx').then((res) => {
+    console.log(res._worksheets[1]._columns)
+})
